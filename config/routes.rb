d9abbb5630210f3e7f0d047186723d4e3d639c09
ThoughtCow <kuehls.business@gmail.com>
2020-08-25 13:29:24 -0600

    You can delete articles, links to all actions in articles page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'details', to: 'pages#details'
   get 'availability', to: 'pages#availability'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
